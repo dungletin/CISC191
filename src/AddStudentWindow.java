8005a99ebb190648912b92cf4a98d16116ab2cc6
Meidyn Nguyen <meidyn.ng@Meidyns-MBP.attlocal.net>
2023-11-05 09:45:02 -0800

    added if statement for combo box to create the specific object

--- src/AddStudentWindow.java
+++ src/AddStudentWindow.java
@@ -21,22 +21,22 @@ public class AddStudentWindow extends JFrame implements ActionListener
 	private JButton addButton;
 	private JButton exitButton;
 	private JPanel southPanel;
-//	private GUIView view;
+	// private GUIView view;
 	private StudentInformation studentInfo;
 	private JComboBox<String> comboBox;
 	private JPanel northPanel;
 	private JLabel titleLabel;
-	
+
 	public AddStudentWindow(StudentInformation studentInfo)
 	{
 		this.studentInfo = studentInfo;
 		createPanel();
 		setLayout(new BorderLayout());
-		add(centerPanel,BorderLayout.CENTER);
+		add(centerPanel, BorderLayout.CENTER);
 		add(southPanel, BorderLayout.SOUTH);
 		add(northPanel, BorderLayout.NORTH);
 		// set the size
-		setSize(300,300);
+		setSize(300, 300);
 
 		// set the program to end when the window is closed
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,63 +55,76 @@ public class AddStudentWindow extends JFrame implements ActionListener
 		nameTextField = new JTextField(10);
 		IDLabel = new JLabel("ID:");
 		IDTextField = new JTextField(10);
-		String[] comboOptions = {"Regular", "Exchange", "Honored", "Tutoring"};
+		String[] comboOptions = { "Regular", "Exchange", "Honored",
+				"Tutoring" };
 		comboBox = new JComboBox<String>(comboOptions);
-		
-		//name panel
+
+		// name panel
 		JPanel namePanel = new JPanel();
 		namePanel.add(nameLabel);
 		namePanel.add(nameTextField);
-		
-		//ID panel
+
+		// ID panel
 		JPanel IDPanel = new JPanel();
 		IDPanel.add(IDLabel);
 		IDPanel.add(IDTextField);
-		
-		//combo box Panel
+
+		// combo box Panel
 		JPanel comboBoxPanel = new JPanel();
 		comboBoxPanel.add(comboBox);
-		
-		//Add components into center panel
-		centerPanel.setLayout(new GridLayout(3,1));
+
+		// Add components into center panel
+		centerPanel.setLayout(new GridLayout(3, 1));
 		centerPanel.add(namePanel);
 		centerPanel.add(IDPanel);
 		centerPanel.add(comboBoxPanel);
 
-		
-		//create south panel
+		// create south panel
 		southPanel = new JPanel();
 		exitButton = new JButton("Exit");
 		addButton = new JButton("Add");
 		addButton.addActionListener(this);
 		southPanel.add(exitButton);
 		southPanel.add(addButton);
-		
-		//create north panel
+
+		// create north panel
 		northPanel = new JPanel();
 		titleLabel = new JLabel("Please enter student infomation:");
 		northPanel.add(titleLabel);
-		
-		
 
 	}
-	
-	
-	
-	
-	
-	
 
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		if(e.getSource() == addButton)
+		if (e.getSource() == addButton)
 		{
-			Student student = new Student(nameTextField.getText(),Integer.parseInt(IDTextField.getText()));
+
+			String name = nameTextField.getText();
+			int ID = Integer.parseInt(IDTextField.getText());
+			String type = (String) comboBox.getSelectedItem();
+			Student student;
+			if (type.equals("Exchange"))
+			{
+				student = new ExchangeStudent(name, ID);
+			}
+			else if (type.equals("Honored"))
+			{
+				student = new HonoredStudent(name,ID);
+			}
+			else if (type.equals("Tutoring"))
+			{
+				student = new TutoringStudent(name,ID);
+			}
+			else
+			{
+				student = new Student(name, ID);
+			}
+
 			try
 			{
 				studentInfo.addStudent(student);
-				
+
 				new GUIView();
 				dispose();
 			}
@@ -120,10 +133,9 @@ public class AddStudentWindow extends JFrame implements ActionListener
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
+
 		}
+
 	}
-	
-	
 
 }
