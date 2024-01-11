7c9a08ce7542e4a0af5b7833bc184f21a969b48a
dungletin <dungle22042000@gmail.com>
2024-01-10 19:00:43 -0800

    deleted comment on a new PC

--- src/Test.java
+++ src/Test.java
@@ -4,8 +4,6 @@ public class Test
 	public static void main(String[] args)
 	{
 		new Controller(new GUIView());
-		// new AddStudentWindow(new StudentInformation());
-		// new GUIView(new StudentInformation());
-
+		
 	}
 }
